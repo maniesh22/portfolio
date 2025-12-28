aa5b72917c048b8cb5a2a6bdd5e9c5abed6264be
Manish-Prajapati <manish.prajapati@ibm.com>
2025-12-28 13:58:20 +0530

    update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles/tailwind.css'
 
-// Type assertion to fix TypeScript error
-const getBasename = () => {
-  if ((import.meta as any).env.DEV) return '/'
-  return '/portfolio-ready/'
-}
-
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={getBasename()}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
