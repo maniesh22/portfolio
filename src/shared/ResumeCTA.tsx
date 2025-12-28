74e1949e7bd5f3dc34740f55353d275fec9e696f
Manish-Prajapati <manish.prajapati@ibm.com>
2025-12-28 16:33:44 +0530

    update

--- src/shared/ResumeCTA.tsx
+++ src/shared/ResumeCTA.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export default function ResumeCTA(){
   return (
     <div className='flex items-center gap-3'>
       <a className='inline-flex items-center gap-2 px-4 py-2 bg-indigo-600 text-white rounded' href='/Manish_Prajapati_Resume.pdf' download>
         Download Resume
       </a>
-      <a className='underline' href='#/projects'>See projects</a>
+      <Link to='/projects' className='hover:underline'>See projects</Link>
     </div>
   )
 }
