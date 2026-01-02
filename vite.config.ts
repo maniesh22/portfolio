98b2cf1a6e3321e8c89eb19c8336d49288d2a48c
Manish-Prajapati <manish.prajapati@ibm.com>
2026-01-02 13:23:06 +0530

    update  vite and package.json

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // CHANGE 1: Remove or comment out the hardcoded base
-  // base: '/portfolio/', 
-  
+  // CHANGE: Default to root (for Docker/Localhost)
+  base: '/', 
   server: {
     host: true, 
     port: 5173,
     hmr: {
-      // CHANGE 2: Remove the hardcoded path. 
-      // Auto-detection works best for Docker/Local unless you have a specific proxy setup.
-      // path: '/portfolio/',
       clientPort: 5173,
     },
     watch: {
